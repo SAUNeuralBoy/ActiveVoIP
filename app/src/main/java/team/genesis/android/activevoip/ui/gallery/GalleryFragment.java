31518a29e2569455c2a4ef9246c82677ddc1ec1a
SAUNeuralBoy <nyrdeyou@126.com>
2021-09-02 01:59:22 +0800

    network test

--- app/src/main/java/team/genesis/android/activevoip/ui/gallery/GalleryFragment.java
+++ app/src/main/java/team/genesis/android/activevoip/ui/gallery/GalleryFragment.java
@@ -16,7 +16,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.IOException;
+
 import team.genesis.android.activevoip.R;
+import team.genesis.network.DNSLookupThread;
+import team.genesis.tunnels.UDPActiveDatagramTunnel;
 
 public class GalleryFragment extends Fragment {
 
@@ -29,17 +33,44 @@ public class GalleryFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
         view.findViewById(R.id.button_test).setOnClickListener(v -> {
             TextView status = view.findViewById(R.id.status_host);
+            status.setTextColor(ContextCompat.getColor(requireContext(),R.color.error_color));
             String hostName = ((EditText)view.findViewById(R.id.input_hostname)).getText().toString().toLowerCase();
             if(hostName.equals("")){
                 status.setText(R.string.hostname_empty);
-                status.setTextColor(ContextCompat.getColor(requireContext(),R.color.error_color));
                 return;
             }
             int port = Integer.valueOf(((EditText)view.findViewById(R.id.input_port)).getText().toString(),10);
             if(port<1||port>65535){
                 status.setText(R.string.port_outrange);
-                status.setTextColor(ContextCompat.getColor(requireContext(),R.color.error_color));
                 return;
+            }
+            DNSLookupThread dns = new DNSLookupThread(hostName);
+            dns.start();
+            try {
+                dns.join(1000);
+            } catch (InterruptedException e) {
+                status.setText(R.string.dns_not_working);
+                return;
+            }
+            if(dns.getIP()==null){
+                status.setText(R.string.unknown_host);
+                return;
+            }
+            int cnt = 0;
+            try {
+                cnt = UDPActiveDatagramTunnel.getProbe(1000).probe(dns.getIP(),port,10);
+            } catch (IOException e) {
+                status.setText(R.string.system_network_failure);
+            }
+            if(cnt<=0)
+                status.setText(R.string.server_no_response);
+            else if(cnt<10) {
+                status.setText(String.format(getString(R.string.packet_loss_by_percent), (10 - cnt) * 10));
+                status.setTextColor(ContextCompat.getColor(requireContext(),R.color.distrubing_color));
+            }
+            else{
+                status.setText(R.string.server_status_fine);
+                status.setTextColor(ContextCompat.getColor(requireContext(),R.color.fine_color));
             }
         });
         return view;
