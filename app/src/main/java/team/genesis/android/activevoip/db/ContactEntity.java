3d126c8d7da27de504909497a8b3685de9d58c87
SAUNeuralBoy <nyrdeyou@126.com>
2021-09-12 17:30:15 +0800

    GSON

--- app/src/main/java/team/genesis/android/activevoip/db/ContactEntity.java
+++ app/src/main/java/team/genesis/android/activevoip/db/ContactEntity.java
@@ -3,6 +3,8 @@ package team.genesis.android.activevoip.db;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.google.gson.Gson;
+
 import team.genesis.android.activevoip.Crypto;
 import team.genesis.android.activevoip.data.Contact;
 
@@ -13,6 +15,9 @@ public class ContactEntity {
     byte[] encryptedData;
     public ContactEntity(Contact contact){
         uuidHash = Crypto.md5(contact.uuid.getBytes());
-
+        encryptedData = Crypto.encryptWithMasterKey(new Gson().toJson(contact).getBytes());
+    }
+    public Contact getContact() throws Crypto.DecryptException {
+        return new Gson().fromJson(new String(Crypto.decryptWithMasterKey(encryptedData)),Contact.class);
     }
 }
