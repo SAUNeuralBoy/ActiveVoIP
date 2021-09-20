6ad973f55a29fddcacabd305450bd7bdbb2acd27
SAUNeuralBoy <nyrdeyou@126.com>
2021-09-20 12:15:41 +0800

    add edit fragment

--- app/src/main/java/team/genesis/android/activevoip/ui/edit/EditFragment.java
+++ app/src/main/java/team/genesis/android/activevoip/ui/edit/EditFragment.java
@@ -1,4 +1,4 @@
-package team.genesis.android.activevoip;
+package team.genesis.android.activevoip.ui.edit;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import team.genesis.android.activevoip.R;
 
 /**
  * A simple {@link Fragment} subclass.
