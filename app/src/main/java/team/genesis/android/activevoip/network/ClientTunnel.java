73fff534fd8fd9eb2685d7b7ba000e16d2d052b1
SAUNeuralBoy <nyrdeyou@126.com>
2021-09-23 23:09:44 +0800

    inherit UDPActiveDatagramTunnel

--- app/src/main/java/team/genesis/android/activevoip/network/ClientTunnel.java
+++ app/src/main/java/team/genesis/android/activevoip/network/ClientTunnel.java
@@ -18,34 +18,26 @@ import team.genesis.tunnels.UDPActiveDatagramTunnel;
 
 import static java.lang.System.exit;
 
-public class ClientTunnel {
+public class ClientTunnel extends UDPActiveDatagramTunnel {
 
     private final Handler writeHandler,recvHandler,dnsHandler;
     private RecvListener mListener;
 
-    private UDPActiveDatagramTunnel tunnel;
-
     private String mHostName;
-    private InetAddress mHost;
 
-    public ClientTunnel(String threadPrefix){
-        try {
-            tunnel = new UDPActiveDatagramTunnel(InetAddress.getLoopbackAddress(),10113,new UUID());
-        } catch (SocketException e) {
-            e.printStackTrace();
-            exit(0);
-        }
+    public ClientTunnel(String threadPrefix,InetAddress hostAddr, int port, UUID src) throws SocketException {
+        super(hostAddr,port,src);
+
         writeHandler = UI.getCycledHandler(threadPrefix +"write");
         recvHandler = UI.getCycledHandler(threadPrefix +"recv");
         dnsHandler = UI.getCycledHandler(threadPrefix +"dns");
         mListener = msg -> {};
         mHostName = "127.0.0.1";
-        mHost=InetAddress.getLoopbackAddress();
         Runnable keepsAlive = new Runnable() {
             @Override
             public void run() {
                 try {
-                    tunnel.keepAlive();
+                    keepAlive();
                 } catch (IOException ignored) {
                 }
                 writeHandler.postDelayed(this,5000);
@@ -56,7 +48,7 @@ public class ClientTunnel {
             @Override
             public void run() {
                 try {
-                    mListener.onRecv(tunnel.recv());
+                    mListener.onRecv(recv());
                 } catch (IOException e) {
                     recvHandler.postDelayed(this,1000);
                     return;
@@ -81,20 +73,19 @@ public class ClientTunnel {
             dns.join(1000);
         } catch (InterruptedException ignored) {
         }
-        if(dns.getIP()!=null&&dns.getIP()!=mHost){
-            mHost = dns.getIP();
-            tunnel.setHost(dns.getIP());
+        if(dns.getIP()!=null){
+            setHost(dns.getIP());
         }
     }
-    public void update(String hostName, int port){
+    public void update(String hostName){
         mHostName = hostName;
-        tunnel.setHost(mHost,port);
         dnsHandler.post(this::updateDNS);
     }
-    public void write(byte[] msg,UUID dst,UUID src){
+    @Override
+    public void send(byte[] msg,UUID dst,UUID src){
         writeHandler.post(()-> {
             try {
-                tunnel.send(msg,dst,src);
+                super.send(msg,dst,src);
             } catch (IOException ignored) {
             }
         });
